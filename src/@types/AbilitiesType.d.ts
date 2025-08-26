a31cbe4e87e359550985c49b8c431480fe4a68d7
Killian <killian.bastardy@gmail.com>
2025-08-26 12:26:36 +0200

    MB-18 début de mise en page de héro details

---
+++ src/@types/AbilitiesType.d.ts
@@ -0,0 +1,10 @@
+export type abilities = {
+    "id": 0,
+    "externalId": 0,
+    "name": "string",
+    "type": "string",
+    "description": "string",
+    "icon": "string",
+    "hero": "string",
+    "isCollab": true
+}
