dd34cfbfe9499cd4d2834c60aeaefb90c8d24d3e
TrqR <tristanqr@gmail.com>
2025-08-27 11:55:02 +0200

    MB-22 redone

--- src/componnents/common/AuthContainer.tsx
+++ src/componnents/common/AuthContainer.tsx
@@ -1,23 +1,32 @@
 import Box from "@mui/material/Box";
-import {Avatar, Button, Paper} from "@mui/material";
+import {Avatar, Button} from "@mui/material";
 import {useState} from "react";
 import ProfileMenu from "./ProfileMenu.tsx";
+import LoginDialog from "./dialogs/LoginDialog.tsx";
+import RegisterDialog from "./dialogs/RegisterDialog.tsx";
 
 const AuthContainer = () => {
     const [logged, setLogged] = useState<boolean>(false)
     const [openMenu, setOpenMenu] = useState(false);
+    const [openLoginDialog, setOpenLoginDialog] = useState<boolean>(false);
+    const [openRegisterDialog, setOpenRegisterDialog] = useState<boolean>(false);
 
     const handleLogout = () => {
         setOpenMenu(false);
         setLogged(false);
     }
 
+    const handleLogin = () => {
+        setOpenLoginDialog(false);
+        setLogged(true);
+    }
+
     return (
         <>
             {!logged ?
                 <Box>
-                    <Button variant={"contained"} onClick={() => setLogged(true)}>Login</Button>
-                    <Button variant={"outlined"}>Register</Button>
+                    <Button variant={"contained"} onClick={() => setOpenLoginDialog(true)}>Login</Button>
+                    <Button variant={"outlined"} onClick={() => setOpenRegisterDialog(true)}>Register</Button>
                 </Box>
                 :
                 <>
@@ -39,10 +48,10 @@ const AuthContainer = () => {
                             </Box>
                         }
                     </Box>
-
                 </>
             }
-
+            <LoginDialog open={openLoginDialog} setOpen={setOpenLoginDialog}></LoginDialog>
+            <RegisterDialog open={openRegisterDialog} setOpen={setOpenRegisterDialog}></RegisterDialog>
         </>
     )
 }
