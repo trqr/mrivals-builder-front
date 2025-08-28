5a21456d3191926797e6fa7404a25ca413ea3fdc
TrqR <tristanqr@gmail.com>
2025-08-28 10:13:57 +0200

    header maj

--- src/componnents/header/Header.tsx
+++ src/componnents/header/Header.tsx
@@ -3,6 +3,7 @@ import Typography from '@mui/material/Typography';
 import {useNavigate} from 'react-router';
 import {AppBar, Button} from "@mui/material";
 import AuthContainer from "../common/AuthContainer.tsx";
+import Box from "@mui/material/Box";
 
 const menuPages = [
     {name: 'Admin', path: '/Admin'},
@@ -11,12 +12,11 @@ const menuPages = [
     {name: 'Acceuil', path: '/'},
 ];
 
-export default function NavBar() {
+export default function Header() {
     const navigate = useNavigate();
 
     return (
-        <AppBar sx={{borderBottom: '3px solid #FDDE2B'}}>
-            <Toolbar>
+        <Box sx={{borderBottom: '3px solid #FDDE2B' , display: "flex", justifyContent: "space-between", alignItems: "center"}}>
                 <Typography variant="h6" noWrap sx={{ flexGrow: 1 }} component="div">
                     Rivals Builder
                 </Typography>
@@ -30,7 +30,6 @@ export default function NavBar() {
                 </Button>
                 ))}
                 <AuthContainer></AuthContainer>
-            </Toolbar>
-        </AppBar>
+        </Box>
     );
 }
--- src/pages/Builder.tsx
+++ src/pages/Builder.tsx
@@ -1,5 +1,5 @@
 import Box from "@mui/material/Box";
-import {Button, Card, CardContent, CardHeader, Grid} from "@mui/material";
+import {AppBar, Button, Card, CardContent, CardHeader, Grid} from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
 import IconButton from "@mui/material/IconButton";
 import {useLoaderData} from "react-router-dom";
