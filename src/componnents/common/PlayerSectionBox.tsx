9b38f3e619c8ab5901db8b57b92073231fbb91d8
Killian <killian.bastardy@gmail.com>
2025-09-08 16:18:18 +0200

    PlayerSectionBox.tsx

--- src/componnents/common/PlayerSectionBox.tsx
+++ src/componnents/common/PlayerSectionBox.tsx
@@ -1,22 +1,30 @@
 import Box from "@mui/material/Box";
 
 const PlayerSectionBox = ({ title, children }: { title: string; children: React.ReactNode }) => (
-    <>
-        <h2 style={{ textAlign: "center", marginBottom: "5px" }}>{title}</h2>
-        <Box style={{
-            border: "solid 2px #FDDE2B",
-            transform: "skew(-21deg)",
+    <Box
+        sx={{
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
-            justifyContent: "space-between",
-            maxWidth: "300px",
-            maxHeight: "300px",
             margin: "10px"
-        }}>
+        }}
+    >
+        <h2 style={{ marginBottom: "5px" }}>{title}</h2>
+        <Box
+            sx={{
+                border: "solid 2px #FDDE2B",
+                transform: "skew(-21deg)",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "space-between",
+                width: "300px",
+                height: "250px"
+            }}
+        >
             {children}
         </Box>
-    </>
+    </Box>
 );
 
 export default PlayerSectionBox;
--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -8,47 +8,30 @@ const Player = () => {
 
     return (
         <Page title={"PlayerPage"} description={"Player Page"}>
-            <Box>
-            <PlayerSectionBox title={"Player info"}>
-                <h1>{player.name}</h1>
-                <p>Level: {player.level}</p>
-                <p>Rank: {player.rank.rank}</p>
-            </PlayerSectionBox>
-                <PlayerSectionBox title={"Ranked Stats}"></PlayerSectionBox>
             <Box
             style={{
-                border: "solid 2px #FDDE2B",
-                transform: "skew(-21deg)",
                 display: "flex",
-                flexDirection: "column",
+                flexDirection: "row",
                 alignItems: "center",
-                justifyContent: "space-between",
-                maxWidth: "300px",
-                maxHeight: "300px",
-                margin: "10px"
+                marginLeft: "30px",
             }}>
-                <p>Total Played : {player.overallStats.ranked.total_matches}</p>
-                <p>Total Win : {player.overallStats.ranked.total_wins}</p>
-                <p>Total MVP : {player.overallStats.ranked.total_mvp}</p>
-                <p>Total SVP : {player.overallStats.ranked.total_svp}</p>
-            </Box>
-            <h2 style={{ textAlign: "center", marginBottom: "5px" }}>Unranked Stats</h2>
-            <Box style={{
-                border: "solid 2px #FDDE2B",
-                transform: "skew(-21deg)",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "space-between",
-                maxWidth: "300px",
-                maxHeight: "300px",
-                margin: "10px"
-            }}>
-                <p>Total Played : {player.overallStats.unranked.total_matches}</p>
-                <p>Total Win : {player.overallStats.unranked.total_wins}</p>
-                <p>Total MVP : {player.overallStats.unranked.total_mvp}</p>
-                <p>Total SVP : {player.overallStats.unranked.total_svp}</p>
-            </Box>
+                <PlayerSectionBox title={"Player info"}>
+                    <h1>{player.name}</h1>
+                    <p>Level: {player.level}</p>
+                    <p>Rank: {player.rank.rank}</p>
+                </PlayerSectionBox>
+                <PlayerSectionBox title={"Ranked Stats"}>
+                    <p>Total Played : {player.overallStats.ranked.total_matches}</p>
+                    <p>Total Win : {player.overallStats.ranked.total_wins}</p>
+                    <p>Total MVP : {player.overallStats.ranked.total_mvp}</p>
+                    <p>Total SVP : {player.overallStats.ranked.total_svp}</p>
+                </PlayerSectionBox>
+                <PlayerSectionBox title={"Unranked Stats"}>
+                    <p>Total Played : {player.overallStats.unranked.total_matches}</p>
+                    <p>Total Win : {player.overallStats.unranked.total_wins}</p>
+                    <p>Total MVP : {player.overallStats.unranked.total_mvp}</p>
+                    <p>Total SVP : {player.overallStats.unranked.total_svp}</p>
+                </PlayerSectionBox>
             </Box>
         </Page>
     );
