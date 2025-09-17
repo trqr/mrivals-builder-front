7a81e6e739c2f590ea0451b9901b83f523476479
TrqR <tristanqr@gmail.com>
2025-09-17 17:04:47 +0200

    MB-58 List done / Header done

--- src/componnents/administration/heroesManagement/HeroesManagement.tsx
+++ src/componnents/administration/heroesManagement/HeroesManagement.tsx
@@ -62,7 +62,7 @@ const HeroesManagement = () => {
         setOpenMatchUpDialog,
     });
 
-    const paginationModel = {page: 0, pageSize: 10};
+    const paginationModel = {page: 0, pageSize: 5};
 
     const handleRoleChaning = async () => {
         await updateHeroesMainRole(selectedRows, selectedRole);
@@ -83,8 +83,34 @@ const HeroesManagement = () => {
                     pageSizeOptions={[5, 10]}
                     checkboxSelection
                     onRowSelectionModelChange={handleSelectionChange}
-                    sx={{border: 0, scrollbarWidth: "thin",
-                        scrollbarColor: `${theme.palette.secondary.main} transparent`,}}
+                    sx={{border: "0", scrollbarWidth: "thin",
+                        scrollbarColor: `${theme.palette.secondary.main} transparent`,
+                        "& .MuiDataGrid-cell": {
+                            display: "flex",
+                            alignItems: "center",
+                        },
+
+                        "& .MuiDataGrid-row:nth-of-type(even)": {
+                            backgroundColor: "rgba(255, 255, 0, 0.05)",
+                        },
+                        "& .MuiDataGrid-row:nth-of-type(odd)": {
+                            backgroundColor: "transparent",
+                        },
+
+                        "& .MuiDataGrid-row:hover": {
+                            backgroundColor: "rgba(255, 255, 0, 0.15) !important",
+                        },
+                        "& .MuiDataGrid-columnHeaders, & .MuiDataGrid-columnHeader": {
+                            backgroundColor: `${theme.palette.background.paper} !important`,
+                        },
+                        "& .MuiDataGrid-columnHeaderTitle": {
+                            color: `${theme.palette.common.white} !important`,
+                            textAlign: "center",
+                            fontWeight: "600",
+                        },}}
+                    rowHeight={70}
+
+
                 />
             </Paper>
 
