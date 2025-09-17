7b741717eb8d90320c2913c8c2da5dc4babf33bc
TrqR <tristanqr@gmail.com>
2025-09-17 15:50:51 +0200

    MB-58 List done / Header done

--- src/pages/layout/Header.tsx
+++ src/pages/layout/Header.tsx
@@ -3,6 +3,9 @@ import {useNavigate} from 'react-router';
 import AuthBox from "../../componnents/common/AuthBox.tsx";
 import Box from "@mui/material/Box";
 import MainButton from "../../componnents/common/buttons/MainButton.tsx";
+import {Button, IconButton, Menu, MenuItem} from "@mui/material";
+import {useState} from "react";
+import MenuIcon from '@mui/icons-material/Menu';
 
 const menuPages = [
     {name: 'Home', path: '/'},
@@ -12,10 +15,50 @@ const menuPages = [
 
 export default function Header() {
     const navigate = useNavigate();
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+
+
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
     return (
         <>
-            <Box sx={{display: "flex", justifyContent: "space-between", alignItems: "center", padding: "0 10px"}}>
+            <Box sx={{display: 'flex', justifyContent: "space-between", alignItems: "center", padding: "0 10px"}}>
+                <Box sx={{ display: { xs: 'flex', sm: 'flex', md: 'none', lg: 'none', xl: 'none' }}}>
+                    <IconButton
+                        id="demo-positioned-button"
+                        aria-controls={open ? 'demo-positioned-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                        onClick={handleClick}
+                    >
+                        <MenuIcon></MenuIcon>
+                    </IconButton>
+                    <Menu
+                        id="demo-positioned-menu"
+                        aria-labelledby="demo-positioned-button"
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'left',
+                        }}
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'left',
+                        }}
+                    >
+                        {menuPages.map((menuItem) => (
+                            <MenuItem onClick={() => navigate(menuItem.path)}>{menuItem.name}</MenuItem>
+                        ))}
+                    </Menu>
+                </Box>
                 <Box
                     sx={{display: "flex", justifyContent: "space-between", alignItems: "center", cursor: "pointer"}}
                     onClick={() => navigate('/')}
@@ -25,7 +68,7 @@ export default function Header() {
                     </Typography>
                 </Box>
 
-                    <Box>
+                    <Box sx={{ display: { xs: 'none', sm: 'none', md: 'flex', lg: 'flex', xl: 'flex' } }}>
                         {menuPages.map((page) => (
                         <MainButton
                             key={page.name}
