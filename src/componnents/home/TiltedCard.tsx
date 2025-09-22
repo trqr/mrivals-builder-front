154a70124a7fa1847613a89af97b527ece692985
Killian <killian.bastardy@gmail.com>
2025-09-22 15:07:53 +0200

    update Home 22/09

--- src/componnents/home/TiltedCard.tsx
+++ src/componnents/home/TiltedCard.tsx
@@ -17,7 +17,7 @@ export default function TiltedCard({
                                        imageHeight = '300px',
                                        imageWidth = '300px',
                                        scaleOnHover = 1.1,
-                                       rotateAmplitude = 14,
+                                       rotateAmplitude = 20,
                                        showMobileWarning = true,
                                        showTooltip = true,
                                        overlayContent = null,
